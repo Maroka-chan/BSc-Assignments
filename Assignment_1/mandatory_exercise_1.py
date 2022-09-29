de52f0491020bbcdae34c7ed45537790b1871b33
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2022-09-29 18:10:43 +0200

    Refactor

--- Assignment_1/mandatory_exercise_1.py
+++ Assignment_1/mandatory_exercise_1.py
@@ -1,50 +1,60 @@
 from random import randint
 from math import log
 
+# Our predefined values
 g = 666
 p = 6661
+bob_pk = 2227
+message = 2000
 
-Bob_pk = 2227
 
-Alice_sk = randint(1,p)
-Alice_pk = (g**Alice_sk) % p
-message_from_alice = 2000
+def gen_pubkey(base, sk, prime):
+    return (base ** sk) % prime
+
+def gen_keypair(base, prime):
+    sk = randint(1, prime)
+    return (gen_pubkey(base, sk, prime), sk)
 
 def encrypt(message, receiver_pk, sk, prime):
-    return ((receiver_pk**sk) % prime) * message
+    return receiver_pk**sk % prime * message
 
 def decrypt(ciphertext, sender_pk, sk, prime):
-    return (ciphertext * (((sender_pk**sk) ** (prime-2)) % prime) % prime)
+    return ciphertext * sender_pk**(sk * (prime-2)) % prime
 
 def bruteforce_sk(ct, base, prime, pk):
-    sk = 1
-    while sk < p:
-        Bpk = (base**sk) % prime
-        if Bpk == pk:
+    for sk in range(p):
+        if gen_pubkey(base, sk, prime) == pk:
             break
-        sk += 1
     return sk
 
-def modify_message(ciphertext, prime):
-    return (ciphertext * 3) % prime
 
 
-ciphertext = encrypt(message_from_alice, Bob_pk, Alice_sk, p)
+# Alice's Keypair
+(alice_pk, alice_sk) = gen_keypair(g, p)
+
+# Alice's Encrypted Message
+ciphertext = encrypt(message, bob_pk, alice_sk, p)
+
+
+# Part 1
+bob_sk = bruteforce_sk(ciphertext, g, p, bob_pk)
 
 print("1:")
 print("Ciphertext: ", ciphertext)
-Bob_sk = bruteforce_sk(ciphertext, g, p, Bob_pk)
-print("Decrypted Ciphertext: ", decrypt(ciphertext, Alice_pk, Bob_sk, p))
-print("Bob's private key: ", Bob_sk)
+print("Decrypted Ciphertext: ", decrypt(ciphertext, alice_pk, bob_sk, p))
+print("Bob's private key: ", bob_sk)
 print("\n")
+
+
+# Part 2
+modified_message = ciphertext * 3 % p
 
 # (g^(xy) * m) / g^(xy) = (g^(xy) * m) * g^(xy)^-1
 print("2:")
 print("Ciphertext: ", ciphertext)
-print("Decrypted Ciphertext", decrypt(ciphertext, Alice_pk, Bob_sk, p))
-modified_message = modify_message(ciphertext, p)
+print("Decrypted Ciphertext", decrypt(ciphertext, alice_pk, bob_sk, p))
 print("Modified Ciphertext: ", modified_message)
-print("Decrypted Modified Ciphertext", decrypt(modified_message, Alice_pk, Bob_sk, p))
+print("Decrypted Modified Ciphertext", decrypt(modified_message, alice_pk, bob_sk, p))
 
 
 
